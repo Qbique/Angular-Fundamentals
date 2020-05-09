e3351776b0ee87305de81ce345d03a3d210576c5
KONSTANTINOS TSAKNIAS <sirkostas@hotmail.com>
2020-05-09 13:11:09 +0300

    minor update

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "events-app",
+  template: `
+    <h2>Hello Kostas</h2>
+    <img src="/assets/images/20160110_142941.jpg" />
+  `,
+})
+export class EventsAppComponent {
+  title = "ng-fundamentals";
+}
